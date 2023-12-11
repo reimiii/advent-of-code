b75910ed8ac09922c3528fbbd3abba5a0826c59d
Hilmi AM <imiia75775@proton.me>
2023-12-11 23:29:22 +0700

    day1 part 2 done

--- src/test/java/franxx/code/year/Y2015.java
+++ src/test/java/franxx/code/year/Y2015.java
@@ -13,6 +13,7 @@ public class Y2015 {
         Scanner scanner = new Scanner(file);
 
         int floor = 0;
+//        int position = 0;
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
 
@@ -21,6 +22,11 @@ public class Y2015 {
                     floor++;
                 } else if (line.charAt(i) == ')') {
                     floor--;
+                }
+
+                if (floor == -1) {
+                    System.out.println("First Position : " + (i + 1));
+                    break;
                 }
             }
 
@@ -32,7 +38,7 @@ public class Y2015 {
     }
 
     @Test
-    void day2() {
+    void day2() throws FileNotFoundException {
         // 2x3x4
         /*
           Fortunately, every present is a box (a perfect right rectangular prism),
@@ -40,6 +46,69 @@ public class Y2015 {
           easier: find the surface area of the box,
           which is 2*l*w + 2*w*h + 2*h*l. The elves also need a
           little extra paper for each present: the area of the smallest side.
+          They have a list of the dimensions (length l, width w, and height h)
          */
+        File file = new File("input/day2"); // replace with your file name
+        Scanner scanner = new Scanner(file);
+
+        int lineCount = 0;
+        int sum = 0;
+        int sumTwo = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] xes = line.trim().split("x");
+
+            int[] dimensions = new int[xes.length]; // 3
+
+            for (int i = 0; i < xes.length; i++) {
+                dimensions[i] = Integer.parseInt(xes[i]);
+            }
+
+            System.out.println(dimensions[0]);
+            System.out.println(dimensions[1]);
+            System.out.println(dimensions[2]);
+
+            int l = dimensions[0];
+            int w = dimensions[1];
+            int h = dimensions[2];
+
+            int ribbon = l + l + w + w;
+            int wrap = l * w * h;
+            int total = ribbon + wrap;
+            sumTwo += total;
+
+            System.out.println("ribbon: " + ribbon);
+            System.out.println("wrap: " + wrap);
+            System.out.println("total: " + total);
+            final int number = 2;
+
+            int lw = dimensions[0] * dimensions[1];
+            int wh = dimensions[1] * dimensions[2];
+            int hl = dimensions[2] * dimensions[0];
+
+            int lowest = lw;
+
+            if (wh < lowest) {
+                lowest = wh;
+            }
+
+            if (hl < lowest) {
+                lowest = hl;
+            }
+
+            int square = (number * lw) + (number * wh) + (number * hl);
+
+            sum += (square + lowest);
+            System.out.println("low: " + lowest);
+
+            System.out.println("=======================");
+            lineCount++;
+        }
+
+        System.out.println("part 1 - sum: " + sum);
+        System.out.println("Part 2 : " + sumTwo);
+        System.out.println("ALL LINE: " + lineCount);
+        scanner.close();
     }
 }
